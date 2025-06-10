193bd1ced473fde0febe81683f606933624fa91c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-06-10 14:07:42 +0530

    modularize into a function

--- 0-building-generative-ai-powered-applications-with-python/1-blip-image-captioning.py
+++ 0-building-generative-ai-powered-applications-with-python/1-blip-image-captioning.py
@@ -7,13 +7,18 @@ model = BlipForConditionalGeneration.from_pretrained(
     "Salesforce/blip-image-captioning-base"
 )
 
-# Open the image file
-image = Image.open("data/photographer.png")
 
-# Preprocess the image and generate the caption
-inputs = processor(image, return_tensors="pt")
-outputs = model.generate(**inputs)
-caption = processor.decode(outputs[0], skip_special_tokens=True)
+def generate_caption(image):
+    inputs = processor(image, return_tensors="pt")
+    outputs = model.generate(**inputs)
+    caption = processor.decode(outputs[0], skip_special_tokens=True)
+    return caption
 
-# Print the generated caption
-print(f"Caption: '{caption}'")
+
+if __name__ == "__main__":
+    # Open the image file
+    image = Image.open("data/photographer.png")
+    caption = generate_caption(image)
+
+    # Print the generated caption
+    print(f"Caption: '{caption}'")
